799fde40fc61f6349e5026bc44dd04cdf1b6a263
Bacon_Space <thestreamstats@gmail.com>
2020-08-17 12:24:31 -0400

    Testing something 😄

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ $(document).ready(function(event){
    });
 });
 function getData(){
-     $.getJSON('https://api.vid.me/channel/' + $('#inputText').val() + '', function(data){
+     $.getJSON('https://fortnite-api.com/v2/cosmetics/br/' + $('#inputText').val() + '', function(data){
          
-         var avatar = data.avatar;
-            var Username = data.username;
+         var avatar = data.images.smallicon;
+            var Username = data.name;
             var followers = data.followers;
             var totalViews = data.video_views;
-            var ID = data.user_id
+            var ID = data.id
 
          var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid red">'+
-                     '<h1><span class="label label-success">' + Username + '</h1>'+
+                     '<h1><span class="label label-success">Skin: ' + Username + '</h1>'+
                      '<br /><b><span class="label label-success">Followers:' + followers + '</b>'+
                      '<br /><b><span class="label label-success">Total Views:' + totalViews + '</b>'+
                      '<br /><b><span class="label label-info"><font color="White"><a href="https://vid.me/' + Username + '">vid.me/'+ Username +'</a></font></b>'+
                      '<br /><b><span class="label label-danger">ID:' + ID + '</b>'+;
          $('.profile').html(html);
       }).fail(function(data){
-            html = '<h1>A Vid.me user with that name does not exist.';
+            html = '<h1>A  fortnite cid with that name does not exist.';
              $('.profile').html(html);
             return;
       });
