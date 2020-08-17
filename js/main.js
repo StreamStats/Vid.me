10bec0dfe6f9c7165fa4f1c0cceecfb3e096ad08
Bacon_Space <thestreamstats@gmail.com>
2020-08-17 12:55:07 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,19 +8,20 @@ $(document).ready(function(event){
    });
 });
 function getData(){
-     $.getJSON('https://fortnite-api.com/v2/cosmetics/br/' + $('#inputText').val() + '', function(data){
+     $.getJSON('https://benbotfn.tk/api/v1/cosmetics/br/' + $('#inputText').val() + '', function(data){
          
-         var avatar = data.images.smallicon;
+         var avatar = data.icons.icon;
             var Username = data.name;
             var followers = data.followers;
-            var totalViews = data.video_views;
-            var ID = data.id
+            var totalViews = data.Description;
+            var ID = data.id;
+            var rarity = data.rarity;
 
          var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid red">'+
                      '<h1><span class="label label-success">Skin: ' + Username + '</h1>'+
                      '<br /><b><span class="label label-success">Followers:' + followers + '</b>'+
                      '<br /><b><span class="label label-success">Total Views:' + totalViews + '</b>'+
-                     '<br /><b><span class="label label-info"><font color="White"><a href="https://vid.me/' + Username + '">vid.me/'+ Username +'</a></font></b>'+
+                     '<br /><b><span class="label label-info">Rarity:' + rarity+ '</b>'+
                      '<br /><b><span class="label label-danger">ID:' + ID + '</b>'+;
          $('.profile').html(html);
       }).fail(function(data){
